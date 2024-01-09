bf2ce91bef06d67c8982633fb25177569dcbba93
Leonardo Violim <120134853+LeonardoViolim@users.noreply.github.com>
2024-01-09 14:31:07 -0300

    Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -2,7 +2,18 @@ namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
     public class Iphone
+    public class Iphone : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero,string modelo,string imei,int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"instalando {nomeApp} no celular iphone");
+        }
     }
 }
+10 changes: 9 additions & 1 deletion 10
+Models/Nokia.cs
